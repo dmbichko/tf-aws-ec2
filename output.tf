13c64c9aaca68e8f1f3f2c3aefff8c99599951ee
Dzmitry Bichko <dmitrijbichko@gmail.com>
2024-03-23 10:39:18 +0300

    change output file

--- output.tf
+++ output.tf
@@ -4,6 +4,7 @@ output "ec2_public_ip" {
   value       = aws_instance.public.public_ip
 }
 resource "local_sensitive_file" "private_key_output" {
-  sensitive_content = tls_private_key.tls-aws-key.private_key_pem
+  content = tls_private_key.tls-aws-key.private_key_pem
   filename          = "${path.module}/${var.key-name}.pem"
+  file_permission = "0600"
 }
